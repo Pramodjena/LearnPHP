24cad306122dbd875e407a99c5c7965d1f3c38d8
Pramodjena <pramoodjena@gmail.com>
2026-04-14 12:50:20 +0530

    Updated

---
+++ phpcs/15-form/data.php
@@ -0,0 +1,19 @@
+<?php
+
+
+// echo "Happy Odia New Year 2026";
+
+// print_r($_GET);
+print_r($_REQUEST);
+
+// if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["age"]) && isset($_POST["course"])) {
+
+//     $name =  $_POST["name"];
+//     $email =  $_POST["email"];
+//     $age =  $_POST["age"];
+//     $course =  $_POST["course"];
+
+//     echo $name, $age, $email, $course;
+// } else {
+//     echo "Data not found !!";
+// }
